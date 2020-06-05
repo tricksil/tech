9988564e84d8f06401b2be333fe228883d049680
Patrick Silva <patrickeeep12@gmail.com>
2020-06-05 10:43:42 -0300

    First commit of tech

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./App.css";
+import profile from "./assets/patrick-perfil.jpg";
+
+import TechList from './components/TechList';
+
+export default function App() {
+  return <TechList />
+}
